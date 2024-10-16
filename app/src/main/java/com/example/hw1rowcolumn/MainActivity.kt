3ef803b83645732ed0ad1b1755b0394b5d67287c
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-10-16 23:49:52 +0300

    added adapter, viewholder, update main class

--- app/src/main/java/com/example/hw1rowcolumn/MainActivity.kt
+++ app/src/main/java/com/example/hw1rowcolumn/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.hw1rowcolumn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.hw1rowcolumn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val adapter = MainAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.recyclerView.adapter = adapter
+
+        binding.fab.setOnClickListener {
+            adapter.addItems(adapter.itemCount + 1)
+        }
+
+        if (savedInstanceState !=  null) {
+
+        } else {
+            adapter.setItems(listOf(1))
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/hw1rowcolumn/MainViewHolder.kt
@@ -0,0 +1,17 @@
+package com.example.hw1rowcolumn
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hw1rowcolumn.databinding.ActivityMainBinding
+
+class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val text = view.findViewById<TextView>(R.id.text_view_1)
+    val image = view.findViewById<ImageView>(R.id.image_view)
+
+    fun bind(number: Int) {
+        text.text = "$number"
+    }
+}
