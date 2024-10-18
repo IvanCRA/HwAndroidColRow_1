2c32f6bf54d6a0bf7254e54c0af1746050a23c87
IvanCRA <101933328+IvanCRA@users.noreply.github.com>
2024-10-18 15:00:13 +0300

    add FlowState

--- app/src/main/java/com/example/hw1rowcolumn/MainActivity.kt
+++ app/src/main/java/com/example/hw1rowcolumn/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.hw1rowcolumn.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,12 +25,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.recyclerView.adapter = adapter
 
-        adapter.setItems(viewModel.itemArray)
+        lifecycleScope.launchWhenStarted {
+            viewModel.itemArray.collect { items ->
+                adapter.setItems(items)
+            }
+        }
 
         binding.fab.setOnClickListener {
             val newItem = adapter.itemCount + 1
             viewModel.addItem(newItem)
-            adapter.setItems(viewModel.itemArray)
         }
 
 
--- app/src/main/java/com/example/hw1rowcolumn/MainViewModel.kt
+++ app/src/main/java/com/example/hw1rowcolumn/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.hw1rowcolumn
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel : ViewModel() {
-    var itemArray = ArrayList<Int>()
+    private var _itemArray = MutableStateFlow<ArrayList<Int>>(ArrayList())
+    val itemArray: StateFlow<ArrayList<Int>> = _itemArray
     fun addItem(item: Int) {
-        itemArray.add(item)
+        val updateList = ArrayList(_itemArray.value)
+        updateList.add(item)
+        _itemArray.value = updateList
     }
 }
